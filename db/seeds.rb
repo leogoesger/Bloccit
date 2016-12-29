fb8339e1a60a37c8379fbbabb812a4ed6e736dc7
leogoesger <leoq91@gmail.com>
2016-12-28 22:37:41 -0800

    assignment 17 complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,42 @@
- require 'random_data'
+require 'random_data'
 
- # Create Posts
- 50.times do
- # #1
-   Post.create!(
- # #2
-     title:  RandomData.random_sentence,
-     body:   RandomData.random_paragraph
-   )
- end
- posts = Post.all
- 
- # Create Comments
- # #3
- 100.times do
-   Comment.create!(
- # #4
-     post: posts.sample,
-     body: RandomData.random_paragraph
-   )
- end
- 
- puts "Seed finished"
- puts "#{Post.count} posts created"
- puts "#{Comment.count} comments created"
+# Create Posts
+50.times do
+# #1
+	Post.create!(
+	# #2
+	 title:  RandomData.random_sentence,
+	 body:   RandomData.random_paragraph
+	)
+end
+
+Post.find_or_create_by(
+	title: "Das New Title",
+	body: "Das New Body"
+)
+
+posts = Post.all
+
+100.times do
+	Comment.create!(
+	# #4
+		post: posts.sample,
+		body: RandomData.random_paragraph
+	)
+end
+
+myPost = 1
+posts.each do |p|
+	if p.title == "Das New Title"
+		myPost = p
+	end
+end
+
+Comment.find_or_create_by(
+	post: myPost,
+	body: "my post"
+)
+
+puts "Seed finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
